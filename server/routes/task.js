813fd1b7decb1b64d6db6f5770af96ff517f2d00
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-05-27 14:05:48 +0530

    Add status of the response.

--- server/routes/task.js
+++ server/routes/task.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const router= express.Router();
+const router = express.Router();
 const Task = require("../models/task");
-const auth =require("../middleware/auth")
+const auth = require("../middleware/auth");
 
-router.post("/tasks",auth, async (req, res) => {
+router.post("/tasks", auth, async (req, res) => {
   const task = new Task({
     ...req.body,
-    "owner":req.user._id
+    owner: req.user._id,
   });
   try {
     await task.save();
@@ -16,7 +16,7 @@ router.post("/tasks",auth, async (req, res) => {
   }
 });
 
-router.get("/tasks/me",auth, async (req, res) => {
+router.get("/tasks/me", auth, async (req, res) => {
   try {
     const tasks = await Task.find({ owner: req.user._id });
     res.send(tasks);
@@ -25,7 +25,7 @@ router.get("/tasks/me",auth, async (req, res) => {
   }
 });
 
-router.get("/tasks/single/:id",auth, async (req, res) => {
+router.get("/tasks/single/:id", auth, async (req, res) => {
   const _id = req.params.id;
   console.log(_id);
 
@@ -41,7 +41,7 @@ router.get("/tasks/single/:id",auth, async (req, res) => {
   }
 });
 
-router.patch("/tasks/:id",auth, async (req, res) => {
+router.patch("/tasks/:id", auth, async (req, res) => {
   const updates = Object.keys(req.body);
 
   const allowUpdates = ["description", "completed"];
@@ -69,7 +69,7 @@ router.patch("/tasks/:id",auth, async (req, res) => {
   }
 });
 
-router.delete("/tasks/:id",auth, async (req, res) => {
+router.delete("/tasks/:id", auth, async (req, res) => {
   try {
     //should check owner id also
     const task = await Task.findOneAndDelete({
@@ -80,7 +80,7 @@ router.delete("/tasks/:id",auth, async (req, res) => {
     }
     res.send(task);
   } catch (error) {
-    res.send(error);
+    res.status(404).send(error);
   }
 });
 
