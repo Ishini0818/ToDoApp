fcdcafef71a9b336a4cfa8fce4a07ecafe489b9c
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-05-27 20:52:32 +0530

    Add jwt key to env variables.

--- server/models/user.js
+++ server/models/user.js
@@ -1,74 +1,73 @@
-const mongoose=require("mongoose");
-const bcrypt=require("bcryptjs");
-const jwt=require("jsonwebtoken");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const Schema=mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const userSchema=new Schema({
-    name : {
-        type : String,
-        required : true
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+      },
     },
-    email : {
-        type : String,
-        required : true,
-        unique : true,
-        trim : true
-    },
-    age : {
-        type : Number,
-        required : true
-    },
-    password: {
-        type : String,
-        trim : true,
-        required : true,
-    },
-    tokens:[
-        {
-            token:{
-                type:String,
-            }
-        }
-    ]
-})
+  ],
+});
 
-userSchema.pre("save",async function(next){
-    const user=this;
+userSchema.pre("save", async function (next) {
+  const user = this;
 
-    if(user.isModified("password")){
-        user.password= await bcrypt.hash(user.password,8)
-    }
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
 
-    next()
-})
+  next();
+});
 
-userSchema.statics.findByCredentials=async(email,password)=>{
-    const user=await User.findOne({email})
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
 
-    if(!user){
-        throw new Error()
-    }
+  if (!user) {
+    throw new Error();
+  }
 
-    const isMatch=await bcrypt.compare(password,user.password)
+  const isMatch = await bcrypt.compare(password, user.password);
 
-    if(!isMatch){
-        throw new Error()
-    }
+  if (!isMatch) {
+    throw new Error();
+  }
 
-    return user;
+  return user;
+};
 
-}
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_KEY);
+  user.tokens = user.tokens.concat({ token });
 
-userSchema.methods.generateAuthToken=async function(){
-    const user=this;
-    const token=jwt.sign({_id:user._id.toString()},"mysecret")
-    user.tokens=user.tokens.concat({token})
+  await user.save();
+  return token;
+};
 
-    await user.save()
-    return token;
-}
+const User = mongoose.model("User", userSchema);
 
-const User= mongoose.model("User",userSchema)
-
-module.exports=User;
+module.exports = User;
